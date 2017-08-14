7aa140ca0e00ae4cd2a6de75f668e40844016217
smeghanareddy <30747448+smeghanareddy@users.noreply.github.com>
2017-08-14 09:28:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,11 +10,11 @@ app.get('/', function (req, res) {
 });
 
 app.get('article', function (req, res) {
-  ;
+  res.sendFile(path.join(__dirname, 'ui', 'article.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'style.css'));
+  res.sendFile(path.join(__dirname,'ui','style.css'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
